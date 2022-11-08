06bd65671cf39b9a78e5c5f7fa7c5c7ef2e4211d
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-11-08 14:56:25 +0530

    Added query

--- app.py
+++ app.py
@@ -8,51 +8,62 @@ from random import randint
 app = Flask(__name__)
 
 
+@app.route('/')
+def renderLoginPage():
+    res = runQuery("SELECT * FROM event_type")
+    # count=runQuery("select count(*) from participants where participants.event_id = res[0][];")
+    if res == []:
+        return '<h4>No Event Types</h4>'
+    else:
+         return render_template('index.html',events = res)
+
+@app.route('/loginfail')
+def renderLoginFail():
+    return render_template('loginfail.html')
+
+@app.route('/admin')
+def renderAdmin():
+    return render_template('admin.html')
+
+@app.route('/eventType')
+def getEvents():
+    res = runQuery("SELECT *,(SELECT COUNT(*) FROM participants AS P WHERE P.event_id = E.type_id ) AS count FROM event_type AS E;")
+    # print(res)
+    if res == []:
+        return '<h4>No Event Types</h4>'
+    else:
+        return render_template('events.html',events = res)
 
 def runQuery(query):
-	try:
-		db = mysql.connector.connect(
-			host='localhost',
-			database='event',
-			user='root',
-			password='password')
+    try:
+        db = mysql.connector.connect(
+            host='localhost',
+            database='event',
+            user='root',
+            password='password')
 
-		if db.is_connected():
-			print("Connected to MySQL, running query: ", query)
-			cursor = db.cursor(buffered = True)
-			cursor.execute(query)
-			db.commit()
-			res = None
-			try:
-				res = cursor.fetchall()
-			except Exception as e:
-				print("Query returned nothing, ", e)
-				return []
-			return res
+        if db.is_connected():
+            print("Connected to MySQL, running query: ", query)
+            cursor = db.cursor(buffered = True)
+            cursor.execute(query)
+            db.commit()
+            res = None
+            try:
+                res = cursor.fetchall()
+            except Exception as e:
+                print("Query returned nothing, ", e)
+                return []
+            return res
 
-	except Exception as e:
-		print(e)
-		return e
+    except Exception as e:
+        print(e)
+        return e
 
-	db.close()
+    db.close()
 
-	print("Couldn't connect to MySQL")
+    print("Couldn't connect to MySQL")
     #Couldn't connect to MySQL
-	return None
-
-@app.route('/')
-def getEvents():
-	res = runQuery("SELECT * FROM event_type")
-	# print(res)
-	if res == []:
-		return '<h4>No Movies Title</h4>'
-	else:
-		return render_template('events.html',events = res)
-
-
-# @app.route('/')
-# def renderLoginPage():
-#     return render_template('./index.html')
+    return None
 
 
 if __name__ == "__main__":
